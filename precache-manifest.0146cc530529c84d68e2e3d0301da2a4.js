f2de5658a9b109c582904f4f5eb892d2400f2fd5
ClintPy <cmaruti93@gmail.com>
2019-12-13 19:54:40 +0300

    [LP]

---
+++ precache-manifest.0146cc530529c84d68e2e3d0301da2a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910d536913f73a0aa3315cf76621c8da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4c2825985575ffd67e4",
+    "url": "/static/css/main.703275a3.chunk.css"
+  },
+  {
+    "revision": "8b6353895dff52deaecd",
+    "url": "/static/js/2.bd3199d2.chunk.js"
+  },
+  {
+    "revision": "d4c2825985575ffd67e4",
+    "url": "/static/js/main.3cac3e5d.chunk.js"
+  },
+  {
+    "revision": "5b06b92bf662c1e82036",
+    "url": "/static/js/runtime~main.5cda6740.js"
+  }
+]);
